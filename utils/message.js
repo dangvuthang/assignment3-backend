6ece9424c904dd010dccf404f03668ff1f20578d
Vu Thang <dangvuthang97@gmail.com>
2021-01-03 21:55:53 +0700

    Added util class

--- utils/message.js
+++ utils/message.js
@@ -1,4 +1,5 @@
 const dayjs = require("dayjs");
+const users = [];
 
 exports.sendMessage = (username, message) => {
   return {
@@ -6,4 +7,29 @@ exports.sendMessage = (username, message) => {
     message,
     createdAt: dayjs().format("h:mm a"),
   };
+};
+
+exports.userJoin = (id, username, room) => {
+  const user = { id, username, room };
+  users.push(user);
+  return user;
+};
+
+exports.getUser = id => {
+  const user = users.find(user => user.id === id);
+  return user;
+};
+
+exports.userLeft = id => {
+  const index = users.findIndex(user => user.id === id);
+  console.log(index);
+  if (index !== -1) {
+    const user = users[index];
+    users.splice(index, 1);
+    return user;
+  }
+};
+
+exports.getRoomUsers = room => {
+  return users.filter(user => user.room === room);
 };
